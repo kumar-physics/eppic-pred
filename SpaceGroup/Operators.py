9ee3a263779466ee12d63f2598b01d2196da98fd
Kumaran Baskaran <kumar.physics@gmail.com>
2014-12-12 15:36:46 +0100

    Screw component detection added

--- SpaceGroup/Operators.py
+++ SpaceGroup/Operators.py
@@ -4,12 +4,12 @@ Created on Dec 11, 2014
 @author: kumaran
 '''
 
-from numpy import matrix
 from re import findall
 from fractions import Fraction
 from numpy import array
 from numpy import trace
 from numpy import linalg
+from numpy import matrix
 class Operator:
     vector={'X':[1.0,0.0,0.0,0.0],'+X':[1.0,0.0,0.0,0.0],'-X':[-1.0,0.0,0.0,0.0],\
             'Y':[0.0,1.0,0.0,0.0],'+Y':[0.0,1.0,0.0,0.0],'-Y':[0.0,-1.0,0.0,0.0],\
@@ -35,6 +35,54 @@ class Operator:
                              self.operator2vecotor(self.OperatorList[2]),\
                              array([0.0,0.0,0.0,1.0])])
         return self.opmatrix
+    
+    
+    def omega(self,opmatrix):
+        k=self.axisType(opmatrix)
+        op=opmatrix.transpose()[:3].transpose()[:3]
+        t=opmatrix.transpose()[3:].transpose()[:3]
+        nullvec=array([0.0,0.0,0.0])
+        Y=array([nullvec,nullvec,nullvec])
+        for i in range(k):
+            Y+=linalg.matrix_power(op, i)
+        Y1=Y/k
+        t1=t/k
+        a=matrix.dot(t1.transpose(),Y1)
+        return t1
+        
+    def axisType(self,opmatrix):
+        d=linalg.det(opmatrix)
+        t=trace(opmatrix)-1#trace of only operator not including translation
+        if d>0:
+            if t==3:
+                ax=1
+            elif t==-1:
+                ax=2
+            elif t==0:
+                ax=3
+            elif t==1:
+                ax=4
+            elif t==2:
+                ax=6
+            else:
+                ax=0
+        elif d<0:
+            if t==-3:
+                ax=-1
+            elif t==1:
+                ax=-2
+            elif t==0:
+                ax=-3
+            elif t==-1:
+                ax=-4
+            elif t==-2:
+                ax=-6
+            else:
+                ax=0
+        else:
+            ax=100
+        return ax
+            
                 
                 
         
@@ -47,10 +95,9 @@ if __name__=="__main__":
         n=len(l.split(" "))
         if n==2:
             op=l.split(" ")[1]
-            print op,
+            #print op
             m=p.operator2matrix(op)
-            #print m
-            print linalg.det(m),trace(m)
+            print op,p.omega(m),p.axisType(m)
 # testing
 #     p.op2matrix('X+Y+Z-1/3')
 #     p.op2matrix('-X+Y+Z-1/3')
